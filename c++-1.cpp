dce0e53ff85a51c7374ca6be56095405723e7fc0
Pratik Chaubey <choubeyprateek3@gmail.com>
2026-08-21 23:18:28 +0530

    Add C++ practice drills and template code

---
+++ c++-1.cpp
@@ -0,0 +1,22 @@
+//  #include<iostream>
+//  using namespace std;
+
+//  int main(){
+//     cout<<"hello world";
+//     return 0;
+//  }    
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int num1=78,num2=70;
+// //    cout<<"the sum of two numbers is:"<< (num1+num2)<<endl;
+// //    cout<<"the diiffrence of two numbers is"<<(num1-num2)<<endl;
+// //    cout<<"the multiplication of two numbers is:"<<(num1*num2)<<endl;
+// //    cout<<"the division value of two numbers is:"<<(num1/num2)<<endl;
+// //    cout<<"the remeinder of num1 by num2 is:"<<(num1%num2)<<endl;
+// //    cout<<"the value of num1<num2:"<<(num1<num2)<<endl;
+// //    return 0;
+// // }
+
+
---
+++ c++2.cpp
@@ -0,0 +1,178 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=6;
+//     int i=1;
+//     while (i<=10){
+//         cout<<"the multiplication table of 6 contains: "<<a*i<<endl;
+//         i++;
+
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"enter your number;"<<endl;
+//     cin>>a;
+
+//     if(a%2==0){
+//         cout<<"the given number is even:"<<a<<endl;
+//     }    
+//     else if(a%2!=0){
+//         cout<<"the entered number is of odd kind:"<<a<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int a;
+//     cout << "enter your number:" << endl;
+//     cin >> a;
+    
+//     if(a % 2 == 0){  // Check if remainder when divided by 2 is 0
+//         cout << "the given number is even: " << a << endl;
+//     }
+//     else if(a % 2 != 0){  // Check if remainder is NOT 0
+//         cout << "the entered number is odd: " << a << endl;
+//     }
+    
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     int a,b,c;
+//     while(true){
+//         cout<<"enter your number1:";
+//         cin>>a;
+//         cout<<"enter your number 2: ";
+//         cin>>b;
+//         cout<<"enter your number3:";
+//         cin>>c;
+        
+        
+
+//         if(a>b&&a>c){
+//             cout<<" a is the greatest among the three"<<a<<endl;
+//         }    
+//         else if(b>a&&b>c){
+//             cout<<" b is the greatest number of all three"<<b<<endl;
+//         }
+//         else{
+//             cout<<"the greatest number of all is three:"<<c<<endl;
+//         } 
+//         return 0;
+       
+
+       
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int marks;
+// int main(){
+
+//     cout<<"enter your marks:"<<endl;
+//     cin>>marks;
+//     if(marks>90 && marks<=100){
+//         cout<<"you have got grade A with"<<marks<<"marks"<<endl; 
+//     }
+//     else if(marks>80 && marks<=90){
+//         cout<<"you have got grade B with"<<marks<<"marks"<<endl;
+//     }
+//     else if(marks>70 && marks<=80){
+//         cout<<"you have got grade C with"<<marks<<"marks"<<endl;
+//     }    
+//     else if(marks>60 && marks<=70){
+//         cout<<"you have got grade D with"<<marks<<"marks"<<endl;
+//     }    
+//     else{
+//         cout<<"you have FAILED"<<endl<<marks<<endl;
+
+//     }        
+    
+
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"enter the numeric value of day:"<<endl;
+//     cin>>a;
+//     switch(a){
+//         case 0:
+//         cout<<"your day is sunday"<<endl;
+//         break;
+//         case 1:
+//         cout<<"your day is monday"<<endl;
+//         break;
+//         case 2:
+//         cout<<"your day is tuesday"    <<endl;
+//         break;
+//         case 3 :
+//         cout<<"your day is wednesday"    <<endl;
+//         break;
+//         case 4 :
+//         cout<<"your day is thursday"     <<endl;
+//         break;
+//         case 5 :
+//         cout<<"your day is  friday"   <<endl;
+//         break;
+//         case 6:
+//         cout<<"your day is satuday" <<endl;
+//         break;
+// return 0;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter the value of n: "<<endl;
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         cout<< "the values are: "<< i<< endl;
+
+
+
+//     }
+// return 0;    
+// }    
+   
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the value of n: "<<endl;
+//     cin>>n;
+//     for(int i=150;i>=n;i--){
+//         cout<<" the decreasing values are in the order:"<< i<<endl;
+
+//     }
+
+// return 0;
+
+// }
+
+
+
+
